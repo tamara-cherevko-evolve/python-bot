3364f983c5620b07726150ad71108a190414d835
tamara-cherevko <tamara.cherevko.mail@gmail.com>
2024-07-09 10:09:33 +0300

    return binance

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from time import sleep
-# from binance.client import Client
+from binance.client import Client
 from flask import Flask, jsonify, render_template 
 from flask_cors import CORS 
 from waitress import serve
@@ -13,31 +13,31 @@ from orders import recalculate_sell_order, start_listening_orders
 app = Flask(__name__) 
 CORS(app)
  
-# client = Client(api_key, api_secret) 
+client = Client(api_key, api_secret) 
 
  
 @app.route('/')
 def index():
     return jsonify({"status": "index"}), 200
 
-# @app.route('/start-dca-grid', methods=['POST'])
-# def start_dca_grid():  
-#     start_DCA_grid(client) 
-#     sleep(1)
-#     return jsonify({"status": "DCA grid started"}), 200
+@app.route('/start-dca-grid', methods=['POST'])
+def start_dca_grid():  
+    start_DCA_grid(client) 
+    sleep(1)
+    return jsonify({"status": "DCA grid started"}), 200
 
-# @app.route('/start-listen-orders', methods=['GET'])
-# def start_listen():  
-#     start_listening_orders(client)
-#     return jsonify({"status": "Listening orders started"}), 200 
-
-
-# @app.route('/recalculate-sell-order', methods=['POST'])
-# def recalculate_order():  
-#     recalculate_sell_order(client) 
-#     sleep(1)
-#     return jsonify({"status": "Order recalculated"}), 200
+@app.route('/start-listen-orders', methods=['GET'])
+def start_listen():  
+    start_listening_orders(client)
+    return jsonify({"status": "Listening orders started"}), 200 
 
 
-# if __name__ == '__main__':
-#     serve(app, host='0.0.0.0', port=8000) 
+@app.route('/recalculate-sell-order', methods=['POST'])
+def recalculate_order():  
+    recalculate_sell_order(client) 
+    sleep(1)
+    return jsonify({"status": "Order recalculated"}), 200
+
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8000) 
