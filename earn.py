2a9c08255593c9fb97c9d14545e13717edee54c3
tamara-cherevko <tamara.cherevko.mail@gmail.com>
2024-10-12 16:17:28 +0300

    lowest_earning_coin

--- earn.py
+++ earn.py
@@ -7,10 +7,8 @@ def select_coin_for_suggestion():
     all_data = get_all_earn_data()
     # Find the coin with the lowest earnings total
     lowest_earning_coin = min(all_data.items(), key=lambda x: Decimal(x[1]['diff_in_percent']))
-
-    # Return the coin with the highest priority
-    print(f"Coin with the lowest earnings: {lowest_earning_coin}")
-    return lowest_earning_coin
+ 
+    return lowest_earning_coin[1]
 
 # Example usage
 if __name__ == "__main__":
